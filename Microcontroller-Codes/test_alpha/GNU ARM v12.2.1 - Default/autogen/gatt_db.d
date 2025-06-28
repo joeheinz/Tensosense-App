e21c9fb1e63737161c4786827838ca622a76fe56
José Heinz <95142558+joeheinz@users.noreply.github.com>
2025-06-28 19:58:14 +0200

    Delete Microcontroller-Codes/test_alpha directory

--- Microcontroller-Codes/test_alpha/GNU ARM v12.2.1 - Default/autogen/gatt_db.d
+++
@@ -1,5 +0,0 @@
-autogen/gatt_db.o: ../autogen/gatt_db.c \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sli_bt_gattdb_def.h
-/Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sli_bt_gattdb_def.h:
--- Microcontroller-Codes/test_alpha/GNU ARM v12.2.1 - Default/gecko_sdk_4.4.2/hardware/driver/imu/src/sl_imu_math.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.4.2/hardware/driver/imu/src/sl_imu_math.o: \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2/hardware/driver/imu/src/sl_imu_math.c \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//hardware/driver/imu/inc/sl_imu.h \
- /Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h
-/Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//hardware/driver/imu/inc/sl_imu.h:
-/Users/joseheinz/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h:
